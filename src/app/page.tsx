a505f1917887c81152e712ae0edef560b4a93de1
Sandeep Shetty <sandeepshettysigadalu@gmail.com>
2025-10-23 17:20:30 +0530

    Added auth pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 
+import TabsDemo from "@/components/TabsDemo";
 import { ModeToggle } from "@/components/ui/ModeToggle";
 
 export default function Home() {
   return (
-    <div>
-      hey
+    <div className="flex items-center justify-center h-screen">
       <div className="absolute top-0 right-0 p-4">
         <ModeToggle />
       </div>
+      <TabsDemo />
     </div>
   );
 }
